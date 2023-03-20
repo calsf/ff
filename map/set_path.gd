88c372c6f88344d88981772267e9102059ff8709
calsf <lefrogjones@gmail.com>
2023-03-19 20:43:47 -0400

    add path variations

--- map/set_path.gd
+++ map/set_path.gd
@@ -5,7 +5,10 @@ const START_POS = Vector2(320, 240)
 onready var _map_area = get_tree().get_root().get_node("Map/MapArea")
 onready var _paths = [
 	load("res://map/PathOne.tscn"),
-	load("res://map/PathTwo.tscn")
+	load("res://map/PathTwo.tscn"),
+	load("res://map/PathThree.tscn"),
+	load("res://map/PathFour.tscn"),
+	load("res://map/PathFive.tscn")
 ]
 
 func _ready():
